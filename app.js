6f64f5e284f92f51cf7cf7bfb936e56894adfacc
Diana Coelho <dianasouza@MacBook-Pro-de-Diana.local>
2019-06-03 01:04:38 -0300

    Configuração das rotas e pasta views

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require('express'),
+    app = express(),
+    request = require('request');
+
+app.set("view engine", "ejs");
+
+
+
+app.get("/websites", (req, res)=>{
+    request("https://jsonplaceholder.typicode.com/users", (error, response, body) =>{
+        if (!error && response.statusCode == 200){
+            var website = JSON.parse(body)
+            res.render("websites", {website: website});
+        }
+    });
+});
+
+app.get("/contatos", (req, res)=>{
+    request("https://jsonplaceholder.typicode.com/users", (error, response, body) =>{
+        if (!error && response.statusCode == 200){
+            var contatos = JSON.parse(body);
+            res.render("contatos", {contatos: contatos});
+        }
+    });
+});
+
+app.get("/enderecos", (req, res)=>{
+    request("https://jsonplaceholder.typicode.com/users", (error, response, body) =>{
+        if (!error && response.statusCode == 200){
+            var enderecos = JSON.parse(body);
+            res.render("enderecos", {enderecos:enderecos});
+        }
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+  });
